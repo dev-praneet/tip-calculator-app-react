e356eaacca8ef4d376a907cbd8e73bca9156d3d6
Geeky Amat <geeky.amat@gmail.com>
2021-12-08 01:28:03 +0530

    logic added and working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 
 
@@ -35,30 +36,76 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
+const Reset = styled.button`
+  padding: 0.4em 0;
+  width: 100%;
+  font-family: 'Space Mono', monospace;
+  font-size: 1rem;
+  font-weight: 700;
+  color: ${props => props.theme.color.hoveredText};
+  background-color: ${props => props.theme.color.lightCyan};
+  outline: none;
+  border: none;
+  transition-property: background-color;
+  transition-duration: 200ms;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${props => props.theme.color.hoveredInputBackground};
+  }
+
+`;
+
 function App() {
+
+  const [billAmount, setBillAmount] = useState('');
+  const [taxPercent, setTaxPercent] = useState('');
+  const [personCount, setPersonCount] = useState('');
+  const [tipPerPerson, setTipPerPerson] = useState(0);
+  const [totalPerPerson, setTotalPerPerson] = useState(0);
+
+  useEffect( () => {
+    if (billAmount != '' && taxPercent != '' && personCount != '' && Number(personCount) != 0) {
+      setTipPerPerson(Number(billAmount) * Number(taxPercent) / 100 / Number(personCount));
+      setTotalPerPerson(Number(billAmount) * (1 + Number(taxPercent) / 100) / Number(personCount));
+      console.log(tipPerPerson, totalPerPerson);
+    } else {
+      setTipPerPerson(0);
+      setTotalPerPerson(0);
+    }
+  }, [billAmount, taxPercent, personCount])
+
+  const clickHandler = (event: React.MouseEvent) => {
+    setBillAmount('');
+    setTaxPercent('');
+    setPersonCount('');
+  }
+
   return (
     <Theme>
       <Wrapper>
         <GlobalStyle/>
         <InputBlock>
           <Caption caption='Bill' />
-          <InputField>
+          <InputField state={billAmount} stateSetter={setBillAmount} personCount={personCount} testRegex={/[^0-9\.]/} alert={false}>
             <IconDollar className='svg-images' viewBox='0 0 11 17'/>
           </InputField>
           <Dummy height={{mobile: '2', desktop: '2'}}/>
           <Caption caption='Select Tip %' />
-          <TaxRates />
+          <TaxRates taxPercent={taxPercent} setTaxPercent={setTaxPercent}/>
           <Dummy height={{mobile: '2', desktop: '2'}}/>
           <Caption caption='Number of People' />
-          <InputField>
+          <InputField state={personCount} stateSetter={setPersonCount} personCount={personCount} testRegex={/[^0-9]/} alert={true}>
             <IconPerson className='svg-images' viewBox='0 0 13 16'/>
           </InputField>
         </InputBlock>
           <Dummy height={{mobile: '2', desktop: '2'}}/>
         <DisplayBlock>
-            <DisplayTab tag='Tip Amount'/>
+            <DisplayTab tag='Tip Amount' amount={tipPerPerson}/>
             <Dummy height={{mobile: '2', desktop: '2'}}/>
-            <DisplayTab tag='Total'/>
+            <DisplayTab tag='Total' amount={totalPerPerson}/>
+            <Dummy height={{mobile: '2', desktop: '2'}}/>
+            <Reset onClick={clickHandler}>RESET</Reset>
         </DisplayBlock>
       </Wrapper>
     </Theme>
