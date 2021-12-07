5993fd8e6a867a80fbed4c44efad652382bace46
Geeky Amat <geeky.amat@gmail.com>
2021-12-08 01:28:51 +0530

    added some colors

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from 'styled-components';
 const theme = {
 color: {
 veryDarkCyan: 'hsl(183, 100%, 15%)',
-darkGrayishCyan1: 'hsl(186, 14%, 43%)',
-darkGrayishCyan2: 'hsl(184, 14%, 56%)',
-lightGrayishCyan1: 'hsl(185, 41%, 64%)',
-lightGrayishCyan2: 'hsl(189, 41%, 97%)',
+inputBackground: 'hsl(202, 50%, 97%)',
+hoveredInputBackground: 'hsl(173, 61%, 77%)',
+lightCyan: 'hsl(171, 67%, 46%)',
+textColor: 'hsl(174, 19%, 34%)',
+hoveredText: 'hsl(182, 100%, 14%)',
+dullCyan: 'hsl(185, 24%, 50%)',
 white: 'hsl(0, 0%, 100%)'
 },
 breakpoint: {
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,10 +4,12 @@ declare module 'styled-components' {
     export interface DefaultTheme {
         color: {
             veryDarkCyan: string,
-            darkGrayishCyan1: string,
-            darkGrayishCyan2: string,
-            lightGrayishCyan1: string,
-            lightGrayishCyan2: string,
+            inputBackground: string,
+            hoveredInputBackground: string,
+            lightCyan: string,
+            textColor: string,
+            hoveredText: string,
+            dullCyan: string,
             white: string
             },
         breakpoint: {
